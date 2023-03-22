1f626de69efb76c3734166fea766bddc63e866db
gm154508 <gm154508@students.berufskolleg-borken.de>
2023-03-22 12:15:57 +0100

    ...

---
+++ SQL-Abfragen.sql
@@ -0,0 +1,23 @@
+
+Abfragen in SQL
+===============
+
+
+Abfrage 1
+------------
+
+Suche alle Kunden aus der Tabelle kunde;
+
+SELECT * FROM kunde;
+
+Das Sternchen steht für alle Eigenschaften (Spalten) aller Kundendatensätze (zeilen)
+
+Abfrage 2
+------------
+
+Suche alle Vornamen und Nachnamen aller Kunden:
+
+SELECT vorname, nachname FROM kunde;
+
+Abfrage  3
+----------
